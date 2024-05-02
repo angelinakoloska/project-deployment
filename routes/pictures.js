12d46c2484042a820e98d64d2f2a89d29f3ea647
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-05-02 07:41:21 +0200

    Add user authentication for pictureDetails

--- routes/pictures.js
+++ routes/pictures.js
@@ -30,17 +30,19 @@ router.get('/', requiresAuth(), async function(req, res, next) {
 });
 
 router.get('/:pictureName', requiresAuth(), async function(req, res, next) {
-  let my_file = await s3.getObject({
-  Bucket: process.env.CYCLIC_BUCKET_NAME,
-  Key: "public/" + req.params.pictureName,
-  }).promise();
-  const picture = {
-  src: Buffer.from(my_file.Body).toString('base64'),
-  name: req.params.pictureName
-  }
-  res.render('pictureDetails', { picture: picture});
-  });
-  
+    let my_file = await s3.getObject({
+        Bucket: process.env.CYCLIC_BUCKET_NAME,
+        Key: req.oidc.user.email + '/' + req.params.pictureName,
+    }).promise();
+
+    const picture = {
+        src: Buffer.from(my_file.Body).toString('base64'),
+        name: req.params.pictureName
+    };
+
+    res.render('pictureDetails', { picture: picture });
+});
+
 
   router.post('/', requiresAuth(), async function(req, res, next) {
     const file = req.files.file;
